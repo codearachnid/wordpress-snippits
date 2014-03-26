c36e7e72cd549c2c08ca863dc96b8cecf1ab6e1c
Timothy Wood <tim@imaginesimplicity.com>
2014-03-26 10:47:30 -0400

    Create taxonomy-radio-vs-checklist.php

---
+++ taxonomy-radio-vs-checklist.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Use radio inputs instead of checkboxes for term checklists in specified taxonomies.
+ *
+ * @param   array   $args
+ * @return  array
+ * @link http://wordpress.stackexchange.com/a/139277/28798
+ * @author THEDEADMEDIC
+ */
+function wp_term_radio_checklist( $args ) {
+    if ( ! empty( $args['taxonomy'] ) && $args['taxonomy'] === 'category' /* <== Change to your required taxonomy */ ) {
+        if ( empty( $args['walker'] ) || is_a( $args['walker'], 'Walker' ) ) { // Don't override 3rd party walkers.
+            if ( ! class_exists( 'WPSE_139269_Walker_Category_Radio_Checklist' ) ) {
+                /**
+                 * Custom walker for switching checkbox inputs to radio.
+                 *
+                 * @see Walker_Category_Checklist
+                 */
+                class WPSE_139269_Walker_Category_Radio_Checklist extends Walker_Category_Checklist {
+                    function walk( $elements, $max_depth, $args = array() ) {
+                        $output = parent::walk( $elements, $max_depth, $args );
+                        $output = str_replace(
+                            array( 'type="checkbox"', "type='checkbox'" ),
+                            array( 'type="radio"', "type='radio'" ),
+                            $output
+                        );
+
+                        return $output;
+                    }
+                }
+            }
+
+            $args['walker'] = new WPSE_139269_Walker_Category_Radio_Checklist;
+        }
+    }
+
+    return $args;
+}
+
+add_filter( 'wp_terms_checklist_args', 'wp_term_radio_checklist' );
